e1e93399ab1c7cb6e14c598e55c1d4d7c7210cd3
ZoNyRina <raeliarinjatovo@gmail.com>
2023-02-28 16:30:34 -0400

    first commit

---
+++ Part1.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.hambutton').click(function(){
+        $('.sidebar').toggleClass('active');
+        $('.sidebar2').toggleClass('active');
+    })
+})
+
+$(document).ready(function(){
+    $('.closebutton,.closebutton2').click(function(){
+        $('.sidebar').toggleClass('active');
+        $('.sidebar2').toggleClass('active');
+    })
+})
+
